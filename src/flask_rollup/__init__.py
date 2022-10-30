3454becea5baa28bacd553ab431cbf632341119c
Jarek Zgoda <jarek.zgoda@gmail.com>
2022-10-30 09:54:02 +0100

    Version 0.3.1

--- src/flask_rollup/__init__.py
+++ src/flask_rollup/__init__.py
@@ -8,7 +8,7 @@ from typing import List, Mapping, Optional, Union
 
 from flask import Flask, request
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 
 def resolve_path(*parts) -> str:  # pragma: no cover
